737e7f8e85b059bd069fe03e2f249f3a9f330648
Henrique Nunes <hrn2001@gmail.com>
2025-03-23 15:04:13 +0000

    Update configs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
   images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
