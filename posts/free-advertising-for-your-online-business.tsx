b32edd9ba13b1c6f50f82689e669cb79b4bf0218
Henrique Nunes <hrn2001@gmail.com>
2025-04-04 19:50:54 +0100

    Make posts more generic

--- posts/free-advertising-for-your-online-business.tsx
+++ posts/free-advertising-for-your-online-business.tsx
@@ -1,51 +1,74 @@
+import { author } from "@/data/authorData";
+import { BlogPost } from "@/types/blogPost";
+import { BlogPostMetadata } from "@/types/blogPostMetadata";
 import Image from "next/image";
 
-const FirstPost = () => {
+const metadata: BlogPostMetadata = {
+  slug: "free-advertising-for-your-online-business",
+  mainImage: "/images/blog/blog-01.png",
+  title: "Free advertising for your online business",
+  metadata:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+  author: author,
+  publishedAt: "July 30, 2023",
+  tags: ["Events"],
+};
+
+
+const Post = async () => {
+  "use server"; // mark function as a server action (fixes the error)
+
   return (
-      <div className="blog-details">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
-          nibh lorem. Duis sed odio lorem. In a efficitur leo. Ut venenatis
-          rhoncus quam sed condimentum. Curabitur vel turpis in dolor volutpat
-          imperdiet in ut mi. Integer non volutpat nulla. Nunc elementum elit
-          viverra, tempus quam non, interdum ipsum.
-        </p>
+    <div className="blog-details">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis nibh
+        lorem. Duis sed odio lorem. In a efficitur leo. Ut venenatis rhoncus
+        quam sed condimentum. Curabitur vel turpis in dolor volutpat imperdiet
+        in ut mi. Integer non volutpat nulla. Nunc elementum elit viverra,
+        tempus quam non, interdum ipsum.
+      </p>
 
-        <p>
-          Aenean augue ex, condimentum vel metus vitae, aliquam porta elit.
-          Quisque non metus ac orci mollis posuere. Mauris vel ipsum a diam
-          interdum ultricies sed vitae neque. Nulla porttitor quam vitae
-          pulvinar placerat. Nulla fringilla elit sit amet justo feugiat
-          sodales. Morbi eleifend, enim non eleifend laoreet, odio libero
-          lobortis lectus, non porttitor sem urna sit amet metus. In
-          sollicitudin quam est, pellentesque consectetur felis fermentum vitae.
-        </p>
+      <p>
+        Aenean augue ex, condimentum vel metus vitae, aliquam porta elit.
+        Quisque non metus ac orci mollis posuere. Mauris vel ipsum a diam
+        interdum ultricies sed vitae neque. Nulla porttitor quam vitae pulvinar
+        placerat. Nulla fringilla elit sit amet justo feugiat sodales. Morbi
+        eleifend, enim non eleifend laoreet, odio libero lobortis lectus, non
+        porttitor sem urna sit amet metus. In sollicitudin quam est,
+        pellentesque consectetur felis fermentum vitae.
+      </p>
 
-        <div className="flex flex-wrap justify-around mb-10">
-          <Image
-            src={"/images/blog/blog-01.png"}
-            width={350}
-            height={200}
-            alt="image"
-          />
-          <Image
-            src={"/images/blog/blog-02.png"}
-            width={350}
-            height={200}
-            alt="image"
-          />
-        </div>
-
-        <h3 className="pt-8">Nunc elementum elit viverra, tempus quam non</h3>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis
-          nibh lorem. Duis sed odio lorem. In a efficitur leo. Ut venenatis
-          rhoncus quam sed condimentum. Curabitur vel turpis in dolor volutpat
-          imperdiet in ut mi. Integer non volutpat nulla. Nunc elementum elit
-          viverra, tempus quam non, interdum ipsum.
-        </p>
+      <div className="mb-10 flex flex-wrap justify-around">
+        <Image
+          src={"/images/blog/blog-01.png"}
+          width={350}
+          height={200}
+          alt="image"
+        />
+        <Image
+          src={"/images/blog/blog-02.png"}
+          width={350}
+          height={200}
+          alt="image"
+        />
       </div>
+
+      <h3 className="pt-8">Nunc elementum elit viverra, tempus quam non</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis nibh
+        lorem. Duis sed odio lorem. In a efficitur leo. Ut venenatis rhoncus
+        quam sed condimentum. Curabitur vel turpis in dolor volutpat imperdiet
+        in ut mi. Integer non volutpat nulla. Nunc elementum elit viverra,
+        tempus quam non, interdum ipsum.
+      </p>
+    </div>
   );
 };
+
+const FirstPost: BlogPost = {
+  metadata: metadata,
+  Component: Post,
+};
+
 export default FirstPost;
