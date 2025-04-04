94f3a534450d39f20afa91494d1b40131dad201b
Henrique Nunes <hrn2001@gmail.com>
2025-04-04 19:50:54 +0100

    Make posts more generic

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -1,23 +1,8 @@
-import { Blog, Author } from "@/types/blog";
+import FirstPost from "@/posts/free-advertising-for-your-online-business";
+import { BlogPost } from "@/types/blogPost";
 
-const author: Author = {
-  name: "Henrique Nunes",
-  image: "/images/blog/author.png",
-  bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
-  _id: 1,
-};
-
-const BlogData: Blog[] = [
-  {
-    _id: 1,
-    slug: "free-advertising-for-your-online-business",
-    mainImage: "/images/blog/blog-01.png",
-    title: "Free advertising for your online business",
-    metadata: "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
-    author: author,
-    publishedAt: "July 30, 2023",
-    tags: ["Events"],
-  },
+const BlogData: BlogPost[] = [
+  FirstPost,
 ];
 
 export default BlogData;
---
+++ data/authorData.tsx
@@ -0,0 +1,9 @@
+import { Author } from "@/types/blogPostMetadata";
+
+
+export const author: Author = {
+	name: "Henrique Nunes",
+	image: "/images/blog/author.png",
+	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+	_id: 1,
+  };
