ed177bf5bc1d528cd6f3db8ce1e269b85b8a116b
Henrique Nunes <hrn2001@gmail.com>
2025-04-09 09:02:58 +0100

    Add markdown render

--- app/(site)/blog/[slug]/page.tsx
+++ app/(site)/blog/[slug]/page.tsx
@@ -44,7 +44,10 @@ const BlogPostPage = async ({ params }) => {
             <div className="lg:w-2/3">
               <div className="animate_top rounded-md border border-stroke bg-white p-7.5 shadow-solid-13 dark:border-strokedark dark:bg-blacksection md:p-10">
                 <PostHeader post={metadata} />
-                <Component />
+                <div className="blog-details">
+                  {/* <MarkdownPost slug={slug} /> */}
+                  <Component />
+                </div>
 
                 {/* <SharePost /> */}
               </div>
